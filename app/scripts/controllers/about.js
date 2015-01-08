2d0fc82225392751e321fb246d0a93f8e5c9a17d
Marco Marchese <marco.marchese75@gmail.com>
2015-01-09 00:09:40 +0100

    Versione 0.1.0
    Nuovo scaffolding app RestFeApp dopo crash con installazione
    UI-Bootstrap.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name restFeV2App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the restFeV2App
+ */
+angular.module('restFeV2App')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name restFeV2App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the restFeV2App
+ */
+angular.module('restFeV2App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
